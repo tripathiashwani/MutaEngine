3cab39bac6d27e165c669474eb8d3159476dbe95
ashwani <tashwani475@gmail.com>
2024-10-28 21:53:27 +0530

    fix:bug at gunicorn

--- gunicorn.sh
+++ gunicorn.sh
@@ -7,6 +7,6 @@ echo "$PWD"
 echo "Migrations started" 
 python3 manage.py makemigrations
 python3 manage.py migrate
-python3 manage.py collectstatic -- no-input
+# python3 manage.py collectstatic -- no-input
 
 python3 manage.py runserver
