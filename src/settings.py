83574a6966d709cee8a3f6a1a7974cc07909af3f
submish <subodhmishra015@gmail.com>
2024-10-29 13:22:18 +0545

    fix: cors allow all origin for development phase

--- src/settings.py
+++ src/settings.py
@@ -227,14 +227,17 @@ CORS_ALLOW_HEADERS = [
 # else:
 #     CSRF_TRUSTED_ORIGINS = []
 
+CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://localhost:3000",
-    "http://127.0.0.1:8000",
-    "https://*",
-    "http://*"
-]
+CORS_ALLOWED_ORIGINS = []
+
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:5173",
+#     "http://localhost:3000",
+#     "http://127.0.0.1:8000",
+#     "https://*",
+#     "http://*"
+# ]
 
 CSRF_TRUSTED_ORIGINS = [
     "http://*",
