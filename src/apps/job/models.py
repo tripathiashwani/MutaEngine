39631a7003722bc0267c8dd724f1ef86b271d587
ashwani <tashwani475@gmail.com>
2024-10-30 11:03:54 +0530

    fix:added field migrations

--- src/apps/job/models.py
+++ src/apps/job/models.py
@@ -89,7 +89,7 @@ class JobTemplate(BaseModel,UserModelMixin):
     job_applicant_template = models.ForeignKey(JobApplicantTemplate, on_delete=models.SET_NULL, null=True, blank=True)
     job_assignment_template = models.ForeignKey(JobAssignmentTemplate, on_delete=models.SET_NULL, null=True, blank=True)
     offer_template = models.ForeignKey(OfferTemplate, on_delete=models.SET_NULL, null=True, blank=True)
-    experience = models.CharField(max_length=255,null=True, blank=True)
+    
 
     def __str__(self):
         return self.title
