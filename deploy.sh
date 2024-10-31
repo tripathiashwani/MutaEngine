7acf7f5fa74af7b7a9e21204db6dd684efedfb07
ashwani <tashwani475@gmail.com>
2024-10-31 13:09:47 +0530

    test

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-PROJECT_DIR="/path/to/backend_career_website"
-VENV_PATH=" venv/bin/activate"
+
 
 screen -S django -X stuff "pkill -f runserver\n"
 screen -S django -X stuff "git fetch origin deployment && git reset --hard origin/deployment && pip install -r requirements.txt && python manage.py migrate && python manage.py collectstatic --noinput && python manage.py runserver 0.0.0.0:8000
