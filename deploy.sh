de36bc86ebcb33618b7dddebc24bcd3f31541a88
ashwani <tashwani475@gmail.com>
2024-10-31 13:06:07 +0530

    fix:command change for the deploy.sh file

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,8 @@ PROJECT_DIR="/path/to/backend_career_website"
 VENV_PATH=" venv/bin/activate"
 
 screen -S django -X stuff "pkill -f runserver\n"
-screen -S django -X stuff "git pull origin deployment && pip install -r requirements.txt && python manage.py migrate && python manage.py collectstatic --noinput && python manage.py runserver 0.0.0.0:8000\n"
+screen -S django -X stuff "git fetch origin deployment && git reset --hard origin/deployment && pip install -r requirements.txt && python manage.py migrate && python manage.py collectstatic --noinput && python manage.py runserver 0.0.0.0:8000
+\n"
 
 
 # check if the pipeline is working testing pipeline test3 for the third time
