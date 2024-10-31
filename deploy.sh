faf8ce3beeadc327e2d3d95bc4b8613d1817a883
ashwani <tashwani475@gmail.com>
2024-10-31 12:45:53 +0530

    testting pipeline

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,4 @@ screen -S django -X stuff "pkill -f runserver\n"
 screen -S django -X stuff "git pull origin deployment && pip install -r requirements.txt && python manage.py migrate && python manage.py collectstatic --noinput && python manage.py runserver 0.0.0.0:8000\n"
 
 
-# check if the pipeline is working
+# check if the pipeline is working testing pipeline
