bffa3f4d48ffa6ab47da3a3deb96633fdcbf9b3a
ashwani <tashwani475@gmail.com>
2024-10-31 12:44:17 +0530

    test

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,6 @@ VENV_PATH=" venv/bin/activate"
 
 screen -S django -X stuff "pkill -f runserver\n"
 screen -S django -X stuff "git pull origin deployment && pip install -r requirements.txt && python manage.py migrate && python manage.py collectstatic --noinput && python manage.py runserver 0.0.0.0:8000\n"
+
+
+# check if the pipeline is working
