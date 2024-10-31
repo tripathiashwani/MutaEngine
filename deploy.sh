4413aa68734c6cc3a2ecc7f33e4d68579ea8abe1
ashwani <tashwani475@gmail.com>
2024-10-31 12:23:45 +0530

    feat:webhook for pipeline

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+PROJECT_DIR="/path/to/backend_career_website"
+VENV_PATH=" venv/bin/activate"
+
+screen -S django -X stuff "pkill -f runserver\n"
+screen -S django -X stuff "git pull origin deployment && source $VENV_PATH && pip install -r requirements.txt && python manage.py migrate && python manage.py collectstatic --noinput && python manage.py runserver 0.0.0.0:8000\n"
