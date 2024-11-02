4806ca81c1dad0c4e9f07b9e7a535262a9547cc9
ashwani <tashwani475@gmail.com>
2024-11-02 15:34:43 +0530

    fix:deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 
 
 screen -S django -X stuff "pkill -f runserver\n"
-screen -S django -X stuff "git pull origin deployment && git reset --hard origin/deployment && pip install -r requirements.txt && python manage.py migrate && python manage.py collectstatic --noinput && python manage.py runserver 0.0.0.0:8000
+screen -S django -X stuff "git fetch origin deployment && git reset --hard origin/deployment && pip install -r requirements.txt && python manage.py migrate && python manage.py collectstatic --noinput && python manage.py runserver 0.0.0.0:8000
 \n"
 
 
