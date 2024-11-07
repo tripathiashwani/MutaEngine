c1b250ba880fe55b877b27dc9117333d564262ec
submish <subodhmishra015@gmail.com>
2024-11-07 20:01:59 +0545

    feat: application id for applicants

--- src/apps/applicant/models.py
+++ src/apps/applicant/models.py
@@ -4,8 +4,10 @@ from src.apps.common.models import BaseModel
 from src.apps.common.utils import get_upload_folder
 from src.apps.job.models import JobTemplate, TemplateExtraField
 from src.apps.auth.models import User
+from django.utils.crypto import get_random_string
 
 class JobApplicant(BaseModel):
+    application_id = models.CharField(max_length=20, null=True, blank=True)
     job_template = models.ForeignKey(JobTemplate, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
@@ -24,6 +26,13 @@ class JobApplicant(BaseModel):
     applicant_accepted = models.BooleanField(default=False)
     manager=models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
     joining_date = models.DateField(null=True, blank=True)
+
+
+    def save(self, *args, **kwargs):
+        if not self.application_id:
+            self.application_id =f"application_{get_random_string(length=6, allowed_chars="0123456789")}"
+
+        return super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
