f717c9477f5b2f4eae75741bfa629e05805ddc13
submish <subodhmishra015@gmail.com>
2024-11-07 19:40:11 +0545

    feat: removed is_delete from all models

--- src/apps/common/models.py
+++ src/apps/common/models.py
@@ -18,6 +18,7 @@ class BaseModel(models.Model):
     )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
 
     class Meta:
         abstract = True
